0fd9732ec1ef3e013cdceafc95ef3a897083342e
k-kohey <kawagutsuchi@gmail.com>
2021-11-20 00:44:18 +0900

    add example app

---
+++ ExampleApp/Shared/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  Shared
+//
+//  Created by k-kohey on 2021/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
