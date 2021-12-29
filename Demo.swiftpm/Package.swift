655a354ca51c9ed4b6c04af4d46e305a7a61193a
k-kohey <kawagutsuchi@gmail.com>
2021-12-29 20:07:50 +0900

    replace with playground app

---
+++ Demo.swiftpm/Package.swift
@@ -0,0 +1,44 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+import AppleProductTypes
+
+let package = Package(
+    name: "Demo",
+    platforms: [
+        .iOS("15.0")
+    ],
+    products: [
+        .iOSApplication(
+            name: "Demo",
+            targets: ["AppModule"],
+            bundleIdentifier: "com.k-kohey.demo",
+            teamIdentifier: "xxxxxxxxxx",
+            displayVersion: "1.0",
+            bundleVersion: "1",
+            supportedDeviceFamilies: [
+                .pad,
+                .phone
+            ],
+            supportedInterfaceOrientations: [
+                .portrait,
+                .landscapeRight,
+                .landscapeLeft,
+                .portraitUpsideDown(.when(deviceFamilies: [.pad]))
+            ]
+        )
+    ],
+    dependencies: [
+        .package(name: "Parchment", path: "../")
+    ],
+    targets: [
+        .executableTarget(
+            name: "AppModule",
+            dependencies: [
+                .productItem(name: "Parchment", package: "Parchment", condition: nil)
+            ]
+        )
+    ]
+)
+
---
+++ Demo.swiftpm/Sources/AppModule/Demo.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+import Parchment
+
+extension LoggerComponentID {
+    static let my: Self = .init("My")
+}
+
+struct MyLogger: LoggerComponent {
+    static var id: LoggerComponentID = .my
+    
+    func send(_ log: [LoggerSendable]) async -> Bool {
+        print("send \(log)")
+        return true
+    }
+}
+
+extension TrackingEvent {
+    static func impletion(_ screen: String) -> Self {
+        TrackingEvent(eventName: "impletion", parameters: ["screen": screen])
+    }
+    
+    static var tap: Self {
+        .init(eventName: "tap", parameters: [:])
+    }
+}
+
+let logger = LoggerBundler(
+    components: [MyLogger()]
+)
+
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Button("send event") {
+                Task {
+                    await logger.send(.tap)
+                }
+            }
+            .onAppear {
+                logger.startLogging()
+                Task {
+                    await logger.send(.impletion("home"))
+                }
+            }
+        }
+    }
+}
+
