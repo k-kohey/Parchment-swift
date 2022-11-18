131c823005dd54e1c1acd6141d0dbeea61211539
k-kohey <kawagutsuchi@gmail.com>
2022-11-18 18:31:22 +0900

    add docc plugin

--- Package.swift
+++ Package.swift
@@ -16,6 +16,9 @@ let package = Package(
             targets: ["ParchmentDefault"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    ],
     targets: [
         .target(
             name: "Parchment",
