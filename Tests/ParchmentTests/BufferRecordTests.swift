9bb0e8cf418b684da2590c0903918d7497e6b5dd
k-kohey <kawagutsuchi@gmail.com>
2023-05-20 03:43:16 +0900

    fix test

--- Tests/ParchmentTests/BufferRecordTests.swift
+++ Tests/ParchmentTests/BufferRecordTests.swift
@@ -9,45 +9,102 @@ import XCTest
 @testable import Parchment
 
 class BufferRecordTests: XCTestCase {
-    func testEncodeAndDecode() throws {
+    func testEcode() throws {
         let originalRecord = BufferRecord(
-            id: "id1",
-            destination: "dest1",
-            eventName: "event1",
-            parameters: [
-                "key1": "value1",
+                id: "id1",
+                destination: "dest1",
+                eventName: "event1",
+                parameters: [
+                    "key1": "value1",
+                    "key2": 123,
+                    "key3": ["subkey1": "subvalue1"],
+                    "key4": ["1", "2", "3"],
+                    "key5": Date(timeIntervalSince1970: 0),
+                    "key6": Data("testdata".utf8),
+                    "key7": 123.4,
+                ],
+                timestamp: Date(timeIntervalSince1970: 0)
+            )
+
+            // Encoding
+            let encoder = JSONEncoder()
+            encoder.dateEncodingStrategy = .iso8601
+            let data = try encoder.encode(originalRecord)
+
+            // Convert encoded data to a JSON string
+            let jsonString = String(data: data, encoding: .utf8)
+
+            // Define the expected JSON string
+            let expectedJSONString = """
+            {
+                "id": "id1",
+                "destination": "dest1",
+                "timestamp": "1970-01-01T00:00:00Z",
+                "eventName": "event1",
+                "parameters": {
+                    "key5": "1970-01-01T00:00:00Z",
+                    "key2": 123,
+                    "key6": "dGVzdGRhdGE=",
+                    "key3": {
+                        "subkey1": "subvalue1"
+                    },
+                    "key7": 123.40000000000001,
+                    "key4": ["1","2","3"],
+                    "key1": "value1"
+                }
+            }
+            """
+            .replacingOccurrences(of: " ", with: "")
+            .replacingOccurrences(of: "\n", with: "")
+
+            // Compare the encoded JSON string with the expected JSON string
+        XCTAssertEqual(jsonString, expectedJSONString)
+    }
+
+    func testDecode() throws {
+        let jsonString = """
+        {
+            "id": "id1",
+            "destination": "dest1",
+            "timestamp": "1970-01-01T00:00:00Z",
+            "eventName": "event1",
+            "parameters": {
+                "key5": "1970-01-01T00:00:00Z",
                 "key2": 123,
-                "key3": ["subkey1": "subvalue1"],
-                "key4": ["1", "2", "3"],
-                "key5": Date(timeIntervalSince1970: 0),
-                "key6": Data("testdata".utf8),
-                "key7": 123.4,
-            ],
-            timestamp: Date(timeIntervalSince1970: 0)
-        )
+                "key6": "dGVzdGRhdGE=",
+                "key3": {
+                    "subkey1": "subvalue1"
+                },
+                "key7": 123.40000000000001,
+                "key4": ["1","2","3"],
+                "key1": "value1"
+            }
+        }
+        """.data(using: .utf8)!
 
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
-
-        // Encoding
-        let encoder = JSONEncoder()
-        encoder.dateEncodingStrategy = .iso8601
-        encoder.dataEncodingStrategy = .deferredToData
-        let data = try encoder.encode(originalRecord)
-
-        // Decoding
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .iso8601
-        decoder.dataDecodingStrategy = .deferredToData
-        let decodedRecord = try decoder.decode(BufferRecord.self, from: data)
+        let result = try decoder.decode(BufferRecord.self, from: jsonString)
 
-        // Check equality
-        XCTAssertEqual(originalRecord.id, decodedRecord.id)
-        XCTAssertEqual(originalRecord.destination, decodedRecord.destination)
-        XCTAssertEqual(originalRecord.eventName, decodedRecord.eventName)
-        XCTAssertEqual(originalRecord.timestamp, decodedRecord.timestamp)
-        XCTAssertEqual(originalRecord.parameters as NSDictionary, decodedRecord.parameters as NSDictionary)
+        print(dump(result))
+
+        XCTAssertEqual(
+            result,
+            BufferRecord(
+                    id: "id1",
+                    destination: "dest1",
+                    eventName: "event1",
+                    parameters: [
+                        "key1": "value1",
+                        "key2": 123,
+                        "key3": ["subkey1": "subvalue1"],
+                        "key4": ["1", "2", "3"],
+                        "key5": "1970-01-01T00:00:00Z",
+                        "key6": "dGVzdGRhdGE=",
+                        "key7": 123.4,
+                    ],
+                    timestamp: Date(timeIntervalSince1970: 0)
+                )
+        )
     }
 }
